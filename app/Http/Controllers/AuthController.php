4bf21238dba50c958e9efab56f23315acfcb52be
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-02-14 09:53:14 +0100

    feat(auth): Add login functionality with API route and AuthController
    - Implemented login functionality in AuthController
    - Added API route for user login in routes/api.php
    - Validates user credentials and returns access token upon success
    - Ensures proper error handling for authentication failures

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Exception;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+
+    public function login(Request $request) {
+        try {
+        $request->validate([
+            'email' => 'required|email',
+                'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'message' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        $token = $user->createToken('accessToken');
+        return response()->json(['accessToken' => $token->plainTextToken],200);
+        } catch(\Exception $e) {
+return response()->json(['message' => $e->getMessage()], 401);
+        }
+    }
+
     public function register(Request $request) {
         try {
             $request->validate([
